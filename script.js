16467b31e575843e7f9d89ea378d828d998e65db
Bruno Souza <80255960+brunoalksouza@users.noreply.github.com>
2023-01-27 13:29:02 +0000

    Adicionei o JS

---
+++ script.js
@@ -0,0 +1,23 @@
+var deadline = new Date("jan 28, 2023 15:37:25").getTime();
+  
+var x = setInterval(function() {
+  
+var now = new Date().getTime();
+var t = deadline - now;
+
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("days").innerHTML =days ;
+document.getElementById("hours").innerHTML =hours;
+document.getElementById("minutes").innerHTML = minutes; 
+document.getElementById("seconds").innerHTML =seconds; 
+
+if (t < 0) {
+        clearInterval(x);
+        document.getElementById("day").innerHTML ='0';
+        document.getElementById("hour").innerHTML ='0';
+        document.getElementById("minute").innerHTML ='0' ; 
+        document.getElementById("second").innerHTML = '0'; }
+}, 1000);
